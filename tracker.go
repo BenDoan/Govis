77655bea25d1d24b05219d3b076fcf6d2f1428b8
Ben Doan <ben@bendoan.me>
2015-05-07 19:15:37 -0500

    added min-diff time config entry

--- tracker.go
+++ tracker.go
@@ -15,6 +15,7 @@ type Tracker struct {
 	currentWindow  string
 	interval       time.Duration
 	minIdleTime    time.Duration
+	minTimeDiff    int
 	ignorePatterns []interface{}
 }
 
@@ -29,8 +30,9 @@ func (t *Tracker) Start(config *jconfig.Config) {
 	}
 
 	t.ignorePatterns = config.GetArray("IgnorePatterns")
-
+	t.minTimeDiff = config.GetInt("MinTimeDiff")
 	t.interval = time.Duration(config.GetInt("TickInterval")) * time.Millisecond
+
 	t.lastTime = time.Now()
 	t.lastWindow = GetCurrentWindowName()
 
@@ -53,7 +55,7 @@ func (t *Tracker) StartTracking() {
 
 func (t *Tracker) PrintStatus() {
 	timeDiff := int((time.Since(t.lastTime)).Seconds())
-	if !(timeDiff < 1) && t.IsValidWindow(t.lastWindow) {
+	if !(timeDiff < t.minTimeDiff) && t.IsValidWindow(t.lastWindow) {
 		fmt.Printf("%vs, %v\n", timeDiff, t.lastWindow)
 		t.lastTime = time.Now()
 	}
@@ -63,7 +65,8 @@ func (t *Tracker) IsValidWindow(lastWindow string) (isValid bool) {
 	isValid = true
 
 	for _, val := range t.ignorePatterns {
-		if str, _ := val.(string); strings.Contains(lastWindow, str) {
+		lowerLastWindow := strings.ToLower(lastWindow)
+		if str, ok := val.(string); ok && strings.Contains(lowerLastWindow, strings.ToLower(str)) {
 			isValid = false
 			break
 		}
